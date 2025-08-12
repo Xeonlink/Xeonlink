038077f2416123389bc95a497f8f667f6c72c7cf
jimin <dia0114@naver.com>
2025-08-12 17:21:13 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { ScrollArea } from './components/ui/scroll-area'
+
+export function App() {
+
+  return (
+    <ScrollArea>
+      <div className="h-full w-full bg-red-500">  sdfsdf</div>
+    </ScrollArea>
+  )
+}
